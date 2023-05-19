de26d100f74af555ee8af517d82c59cc860ad453
parvejkhan-dev <kparvej761@gmail.com>
2023-05-19 11:53:39 +0530

    Redux setup for event App

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Counter } from "./Component/Counters/counter";
+
+
+function App() {
+  return (
+    <>
+      {/* <Counter /> */}
+      <h1>Hello World</h1>
+      <Counter />
+    </>
+  );
+}
+
+export default App;
